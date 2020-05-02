5c7147d8062b6da789a0100d65207a6e68f0d38f
zhaoyanz405 <zhaoyanz405@gmail.com>
2020-05-02 16:26:02 +0800

    add login.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <login></login>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from '@/components/Login'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Login
   }
 }
 </script>
---
+++ src/components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="login_form">
+    <div style="margin: 20px;"></div>
+    <img src="@/assets/logo.png" />
+    <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+      <el-form-item>
+        <el-input v-model="formLabelAlign.username" placeholder="手机/邮箱/用户名"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="formLabelAlign.password" placeholder="密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="register()">注册</el-button>
+        <el-button type="primary" @click="login()">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      labelPosition: "top",
+      formLabelAlign: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    login() {
+      console.log('login')
+    },
+    register() {
+      console.log('register')
+    }
+  }
+};
+</script>
+<style scoped>
+#login_form {
+  width: 400px;
+}
+
+#login_form img {
+  height: 50%;
+  width: 50%;
+}
+</style>
