91ad6fc492863cf1c61e9b6f702d19290e67216c
zhayanz405 <zhayanz405@gmail.com>
2020-05-06 18:18:30 +0800

    add layout and headMenu

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,65 @@
 <template>
-  <div id="app">
-    <login></login>
+  <div id="itemBank">
+    <el-container>
+      <el-header>
+        <head-menu></head-menu>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+          <el-main>
+            <item-bank></item-bank>
+          </el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
   </div>
 </template>
-
-<script>
-import Login from '@/components/Login'
-
-export default {
-  name: 'app',
-  components: {
-    Login
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
 }
 </style>
+<script>
+import itemBank from "@/components/itemBank";
+import headMenu from '@/components/headMenu';
+
+export default {
+  name: "app",
+  components: {
+    itemBank,
+    headMenu
+  }
+};
+</script>
---
+++ src/components/headMenu.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="headMenu">
+    <div class="topNav">
+      <div class="topNavLeft">
+        <img class="logo" src="@/assets/logo.svg" />
+        <p>探索</p>
+        <p>题库</p>
+        <p>圈子</p>
+        <p>竞赛</p>
+        <p>面试</p>
+        <p>商店</p>
+      </div>
+      <div class="topNavRight">
+        <img class="news" src="@/assets/new.svg" />
+        <img class="user" src="@/assets/user.svg" />
+      </div>
+    </div>
+    <!-- <div class="line"></div> -->
+  </div>
+</template>
+<style scoped>
+.topNav {
+  display: flex;
+  height: 50px;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+}
+
+.topNavLeft {
+  height: 100%;
+  display: flex;
+  display: -webkit-flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: center;
+  margin-left: 300px;
+}
+
+.topNavLeft p {
+  margin-right: 15px;
+  text-align: center;
+  font-size: 13px;
+  line-height: 24px;
+}
+
+.topNavRight {
+  height: 100%;
+  display: flex;
+  display: -webkit-flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: center;
+  margin-right: 20px;
+}
+
+.logo {
+  height: 100%;
+  width: 44px;
+}
+
+.news {
+  width: 32px;
+  align-self: flex-start;
+  margin-right: 20px;
+}
+
+.user {
+  height: 100%;
+  width: 26px;
+  margin-right: 300px;
+}
+
+.topNav p {
+  margin-left: 10px;
+  color: #37474f;
+}
+
+.line {
+  height: 1px;
+  width: 100%;
+  background: #e6e6e6;
+}
+</style>
